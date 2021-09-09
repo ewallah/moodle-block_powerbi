8b07da29f5632315d354b005ab2eaa76e97ccb70
Daniel Neis Araujo <danielneis@gmail.com>
2021-09-09 11:33:36 -0300

    bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_powerbi';
-$plugin->release = '7';
-$plugin->version = 2021071400;
+$plugin->release = '8';
+$plugin->version = 2021090900;
 $plugin->requires = 2018120300; // Moodle 3.6.
 $plugin->maturity = MATURITY_STABLE;
