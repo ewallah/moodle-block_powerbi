644977a924f54bb9b8a4b01ebfbe98f2f62b4beb
Daniel Neis Araujo <danielneis@gmail.com>
2022-01-31 17:41:16 -0300

    add null privacy provider

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_powerbi';
-$plugin->release = '10';
-$plugin->version = 2021111800;
+$plugin->release = '11';
+$plugin->version = 2022013100;
 $plugin->requires = 2018120300; // Moodle 3.6.
 $plugin->maturity = MATURITY_STABLE;
